5f97b721123fb170dc0c163474a2bb69d1bb6daf
Hendri <70330701+Kaptencepat@users.noreply.github.com>
2024-07-04 11:21:28 +0700

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -4,35 +4,30 @@
 BACKUP_DIR=/home/adam-user/backup/
 current_date=$(date +%Y-%m-%d) 
 two_days_ago=$(date -d "$current_date -2 days" +%Y-%m-%d)
-namaRS=RSBhaktiHusadaRembang.zip
-zip_filename=$namaRS-$current_date.zip
-zip_file=backup-$current_date.zip
+namaRS="RSBhaktiHusadaRembang"
+zip_filename="$namaRS-$current_date.zip"
+zip_file="backup-$current_date.zip"
 
-docker exec -t db-transaksi-lis pg_dump -c -U postgres -d transaksi > $BACKUP_DIR/transaksi_$current_date.sql
-docker exec -t db-datamaster-lis pg_dump -c -U postgres -d datamaster > $BACKUP_DIR/datamaster_$current_date.sql
-docker exec -t db-archive-lis pg_dump -c -U postgres -d arsip > $BACKUP_DIR/archive_$current_date.sql
-docker exec -t db-pasien-lis pg_dump -c -U postgres -d pasien > $BACKUP_DIR/pasien_$current_date.sql
-docker exec -t db-recyclebin-lis pg_dump -c -U postgres -d recyclebin > $BACKUP_DIR/recyclebin_$current_date.sql
+# Backup each database
+docker exec -t db-transaksi-lis pg_dump -c -U postgres -d transaksi > "$BACKUP_DIR/transaksi_$current_date.sql"
+docker exec -t db-datamaster-lis pg_dump -c -U postgres -d datamaster > "$BACKUP_DIR/datamaster_$current_date.sql"
+docker exec -t db-archive-lis pg_dump -c -U postgres -d arsip > "$BACKUP_DIR/archive_$current_date.sql"
+docker exec -t db-pasien-lis pg_dump -c -U postgres -d pasien > "$BACKUP_DIR/pasien_$current_date.sql"
+docker exec -t db-recyclebin-lis pg_dump -c -U postgres -d recyclebin > "$BACKUP_DIR/recyclebin_$current_date.sql"
 
-cd $BACKUP_DIR
-zip -r "$zip_filename" transaksi_$current_date.sql  datamaster_$current_date.sql archive_$current_date.sql pasien_$current_date.sql recyclebin_$current_date.sql
+# Change to the backup directory
+cd "$BACKUP_DIR"
+
+# Compress the SQL files into a ZIP archive
+zip -r "$zip_filename" transaksi_$current_date.sql datamaster_$current_date.sql archive_$current_date.sql pasien_$current_date.sql recyclebin_$current_date.sql
+
+# Transfer the ZIP archive to the remote server
 sshpass -p 'resman56adam' scp -r "$zip_filename" adam-user@10.8.0.6:/home/adam-user/BackupALL/"$zip_file"
 
-rm transaksi_$current_date.sql  datamaster_$current_date.sql archive_$current_date.sql pasien_$current_date.sql recyclebin_$current_date.sql
+# Remove the SQL files after compression
+rm transaksi_$current_date.sql datamaster_$current_date.sql archive_$current_date.sql pasien_$current_date.sql recyclebin_$current_date.sql
 
-# find "$BACKUP_DIR" -name "backup-*.zip" -type f -ctime +2 -exec rm {} \;
+# Find and delete backup ZIP files older than 2 days
+find "$BACKUP_DIR" -name "$namaRS-*.zip" -type f -mtime +2 -exec rm {} \;
 
-current_timestamp=$(date +%s)
-
-# Menghitung timestamp 2 hari yang lalu
-two_days_ago_timestamp=$((current_timestamp - 2 * 24 * 60 * 60))
-
-# Menggunakan 'find' untuk mencari file ZIP lebih tua dari 2 hari
-# dan menghapusnya dengan 'rm'
-find "$BACKUP_DIR" -name "backup-*.zip" -type f -exec stat -c "%Y %n" {} \; | while read filedate filename
-do
-  if [ "$filedate" -lt "$two_days_ago_timestamp" ]; then
-    rm "$filename"
-    echo "File $filename dihapus."
-  fi
-done
+echo "Backup and cleanup completed successfully."
